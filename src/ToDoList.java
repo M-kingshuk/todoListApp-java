c697216c9adc82bf137ee480518ef55f4234c90f
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-20 21:47:20 +0530

    More tweaks

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -5,27 +5,10 @@ class Task {
     JCheckBox task;
     JButton removeTaskButton;
 
-    public Task(String text, JFrame jFrame, int yPos) {
-        //
-    }
-}
-
-public class ToDoList extends JFrame {
-    public ToDoList() {
-        this.setSize(400, 600);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-
-        // int distTask = 30;
-        // JCheckBox[] tasks = new JCheckBox[1024];
-        
-        JButton addTaskButton = new JButton("Add Task");
-        addTaskButton.setBounds(100, 20, 200, 40);
-        this.add(addTaskButton);
-
+    public Task(String text, JFrame frame, int yPos) {
         JCheckBox task = new JCheckBox("Demo Task");
         task.setBounds(100, 60, 170, 30);
-        this.add(task);
+        frame.add(task);
 
         ImageIcon tempIcon = new ImageIcon("img/binLogo.png");
         Image img = tempIcon.getImage();
@@ -34,7 +17,22 @@ public class ToDoList extends JFrame {
 
         JButton removeTaskButton = new JButton(icon);
         removeTaskButton.setBounds(270, 60, 30, 30);
-        this.add(removeTaskButton);
+        frame.add(removeTaskButton);
+    }
+}
+
+public class ToDoList extends JFrame {
+    public ToDoList() {
+        this.setTitle("To Do List");
+        this.setSize(400, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        Task[] tasks = new Task[1024];
+        
+        JButton addTaskButton = new JButton("Add Task");
+        addTaskButton.setBounds(100, 20, 200, 40);
+        this.add(addTaskButton);
 
         this.setVisible(true);
     }
