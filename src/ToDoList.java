a64bdc8555b1b05d6ac7931191e14222b6a04fa5
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-21 01:22:34 +0530

    App icon, credit and more

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -3,12 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Task {
-    int taskId;
     JCheckBox task;
     JButton removeTaskButton;
 
-    public Task(String text, JFrame frame, int yPos, int tempId) {
-        this.taskId = tempId;
+    public Task(String text, JFrame frame, int yPos) {
         this.task = new JCheckBox(text);
         this.task.setBounds(100, yPos, 170, 30);
         frame.add(this.task);
@@ -22,18 +20,9 @@ class Task {
         this.removeTaskButton.setBounds(270, yPos, 30, 30);
         frame.add(this.removeTaskButton);
 
-        removeTaskButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
-            }
-        });
-
         this.removeTaskButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
-                
                 frame.remove(Task.this.task);
                 frame.remove(Task.this.removeTaskButton);
                 frame.revalidate();
@@ -54,6 +43,17 @@ public class ToDoList extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.getContentPane().setBackground(new Color(198, 217, 238));
+
+        ImageIcon appIcon = new ImageIcon("img/appIcon.png");
+        this.setIconImage(appIcon.getImage());
+
+        JLabel header = new JLabel("Created by Kingshuk and Shourjo");
+        header.setBounds(110, 0, 200, 20);
+        Font tempFont = header.getFont();
+        Font headerFont = tempFont.deriveFont(10f);
+        header.setFont(headerFont);
+        this.add(header);
+
         
         JButton addTaskButton = new JButton("Add Task");
         addTaskButton.setBounds(100, 20, 200, 40);
@@ -65,7 +65,7 @@ public class ToDoList extends JFrame {
                 String taskName = JOptionPane.showInputDialog(ToDoList.this, "Enter task: ", "New Task", JOptionPane.PLAIN_MESSAGE);
 
                 if (taskName != null && !taskName.isBlank()) {
-                    tasks[i++] = new Task(taskName, ToDoList.this, yPos += 30, i - 1);
+                    tasks[i++] = new Task(taskName, ToDoList.this, yPos += 30);
                     ToDoList.this.revalidate();
                     ToDoList.this.repaint();
                 }
