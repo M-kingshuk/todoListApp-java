bf00d7e3ae15bf10a00c3c70a52096e45f8b71b0
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-20 17:06:23 +0530

    small tweaks

--- ToDoList.java
+++ ToDoList.java
@@ -2,10 +2,10 @@ import javax.swing.*;
 
 public class ToDoList {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Hello Swing");
-        frame.setSize(400, 300);
+        JFrame frame = new JFrame("To do list");
+        frame.setSize(400, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new JLabel("Hi from Swing!", SwingConstants.CENTER));
+        frame.add(new JLabel("Lets go, bitch!", SwingConstants.CENTER));
         frame.setVisible(true);
     }
 }
