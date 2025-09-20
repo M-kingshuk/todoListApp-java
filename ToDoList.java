904bca4ff6939d84dc2bd8b74b76b27c098a39e7
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-20 17:02:29 +0530

    Hi Swing

--- ToDoList.java
+++ ToDoList.java
@@ -1,1 +1,11 @@
-// Tf we supposed to do
+import javax.swing.*;
+
+public class ToDoList {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello Swing");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new JLabel("Hi from Swing!", SwingConstants.CENTER));
+        frame.setVisible(true);
+    }
+}
