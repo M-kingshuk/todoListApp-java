7c8dcfc7e4e847add670641caf9f66c321a8850b
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-20 19:26:08 +0530

    Added RemoveTaskButton

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 class Task {
     
@@ -17,6 +18,19 @@ public class ToDoList {
         JButton addTaskButton = new JButton("Add Task");
         addTaskButton.setBounds(100, 20, 200, 40);
         frame.add(addTaskButton);
+
+        JCheckBox task = new JCheckBox("Demo Task");
+        task.setBounds(100, 60, 170, 30);
+        frame.add(task);
+
+        ImageIcon tempIcon = new ImageIcon("img/binLogo.png");
+        Image img = tempIcon.getImage();
+        Image scaledImg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(scaledImg);
+
+        JButton removeTaskButton = new JButton(icon);
+        removeTaskButton.setBounds(270, 60, 30, 30);
+        frame.add(removeTaskButton);
 
         frame.setVisible(true);
     }
