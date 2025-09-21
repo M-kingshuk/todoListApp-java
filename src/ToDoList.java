1bf2eebabe1d476c9fe11afb1a14ffd0a654e269
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-09-21 22:49:49 +0530

    Tasks now are attached to taskPanel

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -7,9 +7,9 @@ class Task {
     JCheckBox task;
     JButton removeTaskButton;
 
-    public Task(String text, JFrame frame, int yPos, ArrayList<Task> tasks) {
+    public Task(String text, JFrame frame, JPanel taskPanel, int yPos, ArrayList<Task> tasks) {
         this.task = new JCheckBox(text);
-        this.task.setBounds(100, yPos, 170, 30);
+        this.task.setBounds(0, yPos, 170, 30);
         frame.add(this.task);
 
         ImageIcon tempIcon = new ImageIcon("img/binLogo.png");
@@ -18,20 +18,20 @@ class Task {
         ImageIcon icon = new ImageIcon(scaledImg);
 
         this.removeTaskButton = new JButton(icon);
-        this.removeTaskButton.setBounds(270, yPos, 30, 30);
+        this.removeTaskButton.setBounds(170, yPos, 30, 30);
         frame.add(this.removeTaskButton);
 
         this.removeTaskButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                taskPanel.remove(Task.this.task);
+                taskPanel.remove(Task.this.removeTaskButton);
                 tasks.remove(Task.this);
-                frame.remove(Task.this.task);
-                frame.remove(Task.this.removeTaskButton);
 
-                int newY = 60;
+                int newY = 0;
                 for (Task currentTask : tasks) {
-                    currentTask.task.setBounds(100, newY, 170, 30);
-                    currentTask.removeTaskButton.setBounds(270, newY, 30, 30);
+                    currentTask.task.setBounds(0, newY, 170, 30);
+                    currentTask.removeTaskButton.setBounds(170, newY, 30, 30);
                     newY += 30;
                 }
 
@@ -43,8 +43,7 @@ class Task {
 }
 
 public class ToDoList extends JFrame {
-    int i = 0;
-    int yPos = 30;
+    int yPos = -30;
     ArrayList<Task> tasks = new ArrayList<>();
 
     public ToDoList() {
@@ -58,7 +57,6 @@ public class ToDoList extends JFrame {
         ImageIcon appIcon = new ImageIcon("img/appIcon.png");
         this.setIconImage(appIcon.getImage());
 
-        // ESHOB ABALCHODNAMO KORBI NA UD-GANDU
         JLabel header = new JLabel("Created by Kingshuk and Shourjo");
         header.setBounds(110, 0, 200, 20);
         Font tempFont = header.getFont();
@@ -70,13 +68,22 @@ public class ToDoList extends JFrame {
         addTaskButton.setBounds(100, 20, 200, 40);
         this.add(addTaskButton);
 
+        JPanel taskPanel = new JPanel();
+        taskPanel.setLayout(null);
+        taskPanel.setBackground(new Color(98, 17, 38));
+        taskPanel.setBounds(100, 60, 200, 500);
+        this.add(taskPanel);
+
         addTaskButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String taskName = JOptionPane.showInputDialog(ToDoList.this, "Enter task: ", "New Task", JOptionPane.PLAIN_MESSAGE);
 
                 if (taskName != null && !taskName.isBlank()) {
-                    tasks.add(new Task(taskName, ToDoList.this, yPos += 30, tasks));
+                    Task newTask = new Task(taskName, ToDoList.this, taskPanel, yPos += 30, tasks);
+                    tasks.add(newTask);
+                    taskPanel.add(newTask.task);
+                    taskPanel.add(newTask.removeTaskButton);
                     ToDoList.this.revalidate();
                     ToDoList.this.repaint();
                 }
